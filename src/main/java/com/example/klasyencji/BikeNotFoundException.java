28546b963fe5918519ce4c1bdc7f8463d4f88db3
NightOtic <121486252+NightOtic@users.noreply.github.com>
2023-02-21 21:36:47 +0100

    init

---
+++ src/main/java/com/example/klasyencji/BikeNotFoundException.java
@@ -0,0 +1,3 @@
+package com.example.klasyencji;
+
+class BikeNotFoundException extends RuntimeException { }
---
+++ src/test/java/com/example/klasyencji/KlasyEncjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.klasyencji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlasyEncjiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
